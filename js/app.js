265384ba87e3427c7682955e99a877cf0b94511a
Shaun Savage <swansavage@gmail.com>
2018-03-05 10:39:19 -0500

    github.io test #2

--- js/app.js
+++ js/app.js
@@ -1,46 +1,61 @@
 // console.log('hello');
 // console.log($);
 
-
-class Hero {
-    constructor(name, dex, firstAttack, secondAttack) {
-      this.name = name
-      this.dex = dex
-      this.firstAttack = firstAttack
-      this.secondAttack = secondAttack
-      this.init = 0
-    }
-    attackOne(){
-      console.log('Player uses Attack 1!');
-    }
-    attackTwo(){
-      console.log('Player uses Attack 2!');
-    }
-    run(){
-      console.log('Player flees the battle!');
-    }
-};
-
-const playerOne = new Hero('Shaun',18,'Charge','Slash' )
-
-playerOne.attackOne();
-playerOne.attackTwo();
-playerOne.run();
-
-
-
-
-
+//================== CLASSES ===================////
 
 $(()=>{
 
+class $Hero {
+    constructor(name, dex, firstAttack, secondAttack) {
+      $(this).name = name
+      $(this).dex = dex
+      $(this).firstAttack = firstAttack
+      $(this).secondAttack = secondAttack
+      $(this).init = 0
+    }
+    attackOne(enemy){
+      console.log($(this).name + ' uses Attack 1!');
+    }
+    attackTwo(enemy){
+      console.log(this.name + ' uses Attack 2!');
+    }
+    run(){
+      console.log(this.name + 'flees the battle!');
+    }
+};
+
+
+
+  //enemy class
+class CompEnemy extends $Hero {
+  constructor(name, dex, firstAttack, secondAttack){
+    super(name, dex, firstAttack, secondAttack)
+  }
+  attackOne(player){
+    console.log(this.name + ' uses attack 1! ');
+  }
+  attackTwo(player){
+    console.log(this.name + ' uses attack 2!');
+  }
+};
+
+const $playerOne = new $Hero('Player 1',18,'Charge','Slash' )
+const guard = new CompEnemy('Guard',14,'Spear','Headbutt');
+
+// guard.attackOne();
 
 
 
 
 
 
+    $('#player-action-1').on('click', $playerOne.attackOne);
+    $('#player-action-2').on('click', $playerOne.attackTwo);
+    $('#player-action-3').on('click', $playerOne.run)
+    $('#comp-action-1').on('click', guard.attackOne);
 
 
+// console.log(guard);
+// console.log(playerOne);
 
 }) //<-- end document.onload
